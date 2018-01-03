a756e1951bc2d960f32fa9fca17f71144dbb6383
atelierkotona <35053003+atelierkotona@users.noreply.github.com>
2018-01-03 12:33:00 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,4 @@
 var swiper = new Swiper('.swiper-container', {
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-  },
-  spaceBetween: 30,
   effect: 'fade',
   autoplay: {
     delay: 5000,
